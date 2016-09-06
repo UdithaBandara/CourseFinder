eb92e5f6be3d77bb8e370da80ce0340dbc23e487
Uditha Bandara <Uditha Bandara>
2016-09-06 13:17:45 +0530

    CourseFinder

---
+++ php/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("sql6.freesqldatabase.com","sql6134572","QriJPQN74B","sql6134572") or die ("could not connect database");
+?>
